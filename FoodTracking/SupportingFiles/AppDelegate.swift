b6ab101dd4cd8e57a8708f7ec32a91b46bbf12a3
Yuriy <yuriy.senchkovskiy@gmail.com>
2020-09-15 23:25:25 +0300

    Готовый проект

--- FoodTracking/SupportingFiles/AppDelegate.swift
+++ FoodTracking/SupportingFiles/AppDelegate.swift
@@ -11,11 +11,23 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var hasAlreadyLaunched :Bool!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        hasAlreadyLaunched = UserDefaults.standard.bool(forKey: "hasAlreadyLaunched")
+        
+        if hasAlreadyLaunched {
+            hasAlreadyLaunched = true
+        } else {
+            UserDefaults.standard.set(true, forKey: "hasAlreadyLaunched")
+        }
+        
         return true
+    }
+    
+    func sethasAlreadyLaunched() {
+        hasAlreadyLaunched = true
     }
 
     // MARK: UISceneSession Lifecycle
@@ -31,7 +43,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
--- FoodTracking/ViewControllers/TabBarViewController.swift
+++ FoodTracking/ViewControllers/TabBarViewController.swift
@@ -12,11 +12,39 @@ class TabBarViewController: UITabBarController {
 
     var user = User.shared
     let foods = Food.getFoods()
+    let userFirstLaunch  = """
+
+        Алексей, добрый день!
+        Мы сделали приложение, которое помогает правильно питаться. При этом оно основывается не на каллориях, а на качестве пищи, которую мы потребляем каждый день.
+        
+        Начать им пользоваться достаточно просто - зайди на первый экран, и ты увидишь, что продукты разделены на полезные и вредные. Про то, что входит в каждую категорию продуктов можно узнать провалившись в описание категории. Добавляй баллы за каждую порцию полезных продуктов или вычитай их из дневного бюджета потребляя вредные. Дневная цель - набрать 80% от максимально возможного количества баллов.
+        На втором экране тебя ждёт круговая диаграмма дневных достижений; сумма полезных баллов, на которую ещё можно рассчитывать сегодня.
+        На третьем экране тебя ожидает экран профиля пользователя с расчётом Индекса Массы Тела, выбора системы измерений, установкой цветовой темы приложения.
+
+        Если впереди нас будет ожидать ещё одна командная работа, мы будем рады доработать данное приложение, добавив загрузку/выгрузку на сервер, создание личного кабинета и графика достижений за неделю/месяц/год.
+
+        """
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupViewControllers(with: user)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if !appDelegate.hasAlreadyLaunched {
+            appDelegate.sethasAlreadyLaunched()
+            displayLicenAgreement(message: self.userFirstLaunch)
+        }
+    }
+    
+    func displayLicenAgreement(message:String){
+        let alert = UIAlertController(title: "О чём наше приложение?", message: message, preferredStyle: .alert)
+        let acceptAction = UIAlertAction(title: "Покажите его скорее!", style: .default)
+        
+        alert.addAction(acceptAction)
+        self.present(alert, animated: true, completion: nil)
     }
     
     private func setupViewControllers(with user: User) {
